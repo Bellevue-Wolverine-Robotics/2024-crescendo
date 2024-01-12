cf56c6f5d7f03508af2797f65ea8438e849ad1fa
Justin Jiang <justinjiang@Justins-MacBook-Pro.local>
2024-01-11 18:05:00 -0800

    dummy function setup

---
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -0,0 +1,20 @@
+package frc.robot.subsystems;
+
+import java.lang.Object;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class DriveSubsystem extends SubsystemBase{
+    public DriveSubsystem() {
+    }
+    public Pose2d getPos(){
+        //TODO: stop retrun dummy
+        return new Pose2d();
+    }
+    public void tankDrive(double left, double right){
+                //TODO: stop retrun dummy
+    }
+    public void arcadeDrive(double left, double right){
+                //TODO: stop retrun dummy
+    }
+}
