1d530c4f1811476937b5aee0c9ddfc40bcbe72bb
Justin Jiang <justinjiang@Justins-MacBook-Pro.local>
2024-01-11 18:40:45 -0800

    MotorControllerGroup deprecated

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -4,6 +4,7 @@ import java.lang.Object;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
+import edu.wpi.first.wpilibj.motorcontrol.PWMMotorController;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.CANConstants;
 import frc.robot.Constants.Throttles;
@@ -16,15 +17,15 @@ import com.revrobotics.CANSparkLowLevel.MotorType;
 public class DriveSubsystem extends SubsystemBase{
     private CANSparkMax m_leftBack = new CANSparkMax(CANConstants.backLeft, MotorType.kBrushless);
     private CANSparkMax m_leftFront = new CANSparkMax(CANConstants.frontLeft, MotorType.kBrushless);
-    private CANSparkMax m_rightFront = new CANSparkMax(CANConstants.frontLeft, MotorType.kBrushless);
+    private CANSparkMax m_rightFront = new CANSparkMax(CANConstants.frontRight, MotorType.kBrushless);
     private CANSparkMax m_rightBack = new CANSparkMax(CANConstants.backRight, MotorType.kBrushless);
 
-    private MotorControllerGroup m_leftGroup = new MotorControllerGroup(m_leftFront, m_leftBack);
-    private MotorControllerGroup m_rightGroup = new MotorControllerGroup(m_rightFront, m_rightBack);
-
-    private DifferentialDrive m_drive = new DifferentialDrive(m_leftGroup, m_rightGroup);
+    private DifferentialDrive m_drive = new DifferentialDrive(m_leftFront, m_rightFront);
 
     public DriveSubsystem() {
+        m_leftBack.follow(m_leftFront);
+        m_rightBack.follow(m_rightFront);
+
         this.m_leftFront.restoreFactoryDefaults();
         this.m_leftBack.restoreFactoryDefaults();
         this.m_rightFront.restoreFactoryDefaults();
@@ -34,9 +35,6 @@ public class DriveSubsystem extends SubsystemBase{
         m_leftBack.setIdleMode(IdleMode.kBrake);
         m_rightFront.setIdleMode(IdleMode.kBrake);
         m_rightBack.setIdleMode(IdleMode.kBrake);
-
-        m_rightFront.setInverted(true);
-        m_rightBack.setInverted(true);
     
 
     }
