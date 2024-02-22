ef0ddf55525f9a43d2c570d761b18800775bae4e
enyoukai <52297896+enyoukai@users.noreply.github.com>
2024-02-22 14:01:28 -0800

    climber tuning

---
+++ src/main/java/frc/robot/commands/climber/ClimberContinuousCommand.java
@@ -0,0 +1,9 @@
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.ClimberSubsystem;
+
+public class ClimberContinuousCommand extends Command {
+
+	ClimberContinuousCommand(ClimberSubsystem climberSubsystem) {
+		
+	}
+}
