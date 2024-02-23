925297eccaa8d6fd18cf7a70dedc69e49bf53f19
Joseph An <52297896+enyoukai@users.noreply.github.com>
2024-02-22 16:01:26 -0800

    removing some unnecessary stuff

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -80,8 +80,6 @@ public class RobotContainer {
       // return Autos.forwardTest(m_driveSubsystem);
       // case CUSTOM_PATH_PLANNER:
       // return Autos.test949PathPlan(m_driveSubsystem);
-      case PATH_PLANNER:
-        return Autos.getPathPlannerCommand();
       default:
         return null;
     }
