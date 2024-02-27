e5e42d7588dd22f79fbe34aa5396183d10d008d1
il798li <svpa2008@gmail.com>
2024-02-26 18:46:49 -0800

    made FullFlywheelShoot command and FeederStartCommand

--- src/main/java/frc/robot/commands/FullFlywheelShoot.java
+++ src/main/java/frc/robot/commands/FullFlywheelShoot.java
@@ -1,16 +1,22 @@
 package frc.robot.commands;
 
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.commands.flywheel.FeederStartCommand;
+import frc.robot.commands.flywheel.FlywheelShootCommand;
+import frc.robot.subsystems.FlywheelSubsystem;
 
 public class FullFlywheelShoot {
     SequentialCommandGroup fullShoot = new SequentialCommandGroup ();
+    private FlywheelSubsystem flywheel;
         
     public FullFlywheelShoot () {
-        fullShoot.addCommands (new FlywheelShootCommand ());
-        fullShoot.addCommand(new FeederStartCommand ());
+        flywheel = new FlywheelSubsystem ();
+        this.fullShoot.addCommands (new FlywheelShootCommand (flywheel));
+        this.fullShoot.addCommands (new FeederStartCommand(flywheel));
     }
     
     public Command command () {
-        return fullShoot;
+        return this.fullShoot;
     }
 }
