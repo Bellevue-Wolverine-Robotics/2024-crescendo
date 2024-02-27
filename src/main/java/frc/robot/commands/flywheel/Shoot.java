f78d62890e19a1e1831a82831ec9325c02d81913
enyoukai <52297896+enyoukai@users.noreply.github.com>
2024-02-26 18:24:41 -0800

    wrote autons

---
+++ src/main/java/frc/robot/commands/flywheel/Shoot.java
@@ -0,0 +1,12 @@
+package frc.robot.commands.flywheel;
+
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+
+public class Shoot {
+	public Command getCommand() {
+		return new SequentialCommandGroup()
+
+	}
+
+}
