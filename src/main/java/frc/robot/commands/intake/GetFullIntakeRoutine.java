e0a9a3767b5a2803462128157d4ccd061b7129c4
Justin Jiang <justin.jiang.co@gmail.com>
2024-02-27 17:54:44 -0800

    added smartdashboard tune for drive subsystem

--- src/main/java/frc/robot/commands/intake/GetFullIntakeRoutine.java
+++ src/main/java/frc/robot/commands/intake/GetFullIntakeRoutine.java
@@ -6,7 +6,6 @@ import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.commands.flywheel.FlywheelStartIntakeCommand;
 import frc.robot.commands.flywheel.FlywheelStopIntakeCommand;
-import frc.robot.commands.flywheel.GetIntakeStatusFlywheel;
 import frc.robot.subsystems.DriveSubsystem;
 import frc.robot.subsystems.FlywheelSubsystem;
 import frc.robot.subsystems.IntakeSubsystem;
