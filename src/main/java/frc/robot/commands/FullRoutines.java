52ca2077fb564af7742cdc35f31918d1bc601c67
enyoukai <52297896+enyoukai@users.noreply.github.com>
2024-02-29 17:49:59 -0800

    fixed autons

--- src/main/java/frc/robot/commands/FullRoutines.java
+++ src/main/java/frc/robot/commands/FullRoutines.java
@@ -4,25 +4,17 @@
 
 package frc.robot.commands;
 
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.BackupPathPlanner;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.commands.flywheel.FlywheelAimIntakeReceiveCommand;
 import frc.robot.commands.flywheel.FlywheelAimSpeakerCommand;
 import frc.robot.commands.flywheel.FlywheelMoveToMakeSpaceForIntakeCommand;
 import frc.robot.commands.intake.DeployIntakeCommand;
-import frc.robot.commands.intake.GetIntakeStatus;
-import frc.robot.commands.intake.StopIntakeCommand;
 import frc.robot.commands.intake.StowIntakeCommand;
-import frc.robot.subsystems.DriveSubsystem;
 import frc.robot.subsystems.FlywheelSubsystem;
 import frc.robot.subsystems.IntakeSubsystem;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import edu.wpi.first.wpilibj2.command.WaitCommand;
 
 public final class FullRoutines {
 
